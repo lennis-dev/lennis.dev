e4a7f7dc02b81c0303b9f961402b105c6ddd7c1f
Lennis <128396922+lennis-dev@users.noreply.github.com>
2024-05-01 21:12:46 +0200

    Changing the font size and improving user-friendliness scroll

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 
-function renderfile(string $file): string
+function renderfile(string $file, bool $ishome = false): string
 {
     $content = file_get_contents($file);
     $master = file_get_contents(__DIR__ . "/pages/_master.html");
     $master = str_replace("[content]", $content, $master);
+    if ($ishome)
+        $master = str_replace("[ishome]", "home", $master);
+    else
+        $master = str_replace("[ishome]", "", $master);
+
     return $master;
 }
 if ($_SERVER["REQUEST_URI"] == "/") {
-    echo renderfile(__DIR__ . "/pages/_index.html");
+    echo renderfile(__DIR__ . "/pages/_index.html", true);
     exit();
 } else if ($_SERVER["REQUEST_URI"] == "/sendmail") {
     header("Location: mailto:x@lennis.dev");
@@ -17,9 +22,9 @@ if ($_SERVER["REQUEST_URI"] == "/") {
     $realpath = realpath(__DIR__ . "/pages/" . explode("/", $_SERVER["REQUEST_URI"])[1] . ".html");
     if (str_starts_with($realpath, __DIR__ . "/pages/")) {
         if (file_exists($realpath)) {
-            echo renderfile($realpath);
+            echo renderfile($realpath, false);
             exit();
         }
     }
 }
-echo renderfile(__DIR__ . "/pages/_404.html");
+echo renderfile(__DIR__ . "/pages/_404.html", false);
