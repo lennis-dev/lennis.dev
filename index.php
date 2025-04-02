df2b3c08d4d4daf0a6c273694ee6ccdd78679a71
Lennis <x@lennis.dev>
2025-04-03 00:01:55 +0200

    add 404 header

--- index.php
+++ index.php
@@ -5,21 +5,24 @@ function renderfile(string $file, bool $ishome = false): string
     $content = file_get_contents($file);
     $master = file_get_contents(__DIR__ . "/pages/_master.html");
     $master = str_replace("[content]", $content, $master);
-    if ($ishome)
+    if ($ishome) {
         $master = str_replace("[ishome]", "home", $master);
-    else
+    } else {
         $master = str_replace("[ishome]", "", $master);
-
+    }
     return $master;
 }
+
 if ($_SERVER["REQUEST_URI"] == "/") {
     echo renderfile(__DIR__ . "/pages/_index.html", true);
     exit();
-} else if ($_SERVER["REQUEST_URI"] == "/sendmail") {
+} elseif ($_SERVER["REQUEST_URI"] == "/sendmail") {
     header("Location: mailto:x@lennis.dev");
-    exit;
+    exit();
 } else {
-    $realpath = realpath(__DIR__ . "/pages/" . explode("/", $_SERVER["REQUEST_URI"])[1] . ".html");
+    $realpath = realpath(
+        __DIR__ . "/pages/" . explode("/", $_SERVER["REQUEST_URI"])[1] . ".html"
+    );
     if (str_starts_with($realpath, __DIR__ . "/pages/")) {
         if (file_exists($realpath)) {
             echo renderfile($realpath, false);
@@ -27,4 +30,6 @@ if ($_SERVER["REQUEST_URI"] == "/") {
         }
     }
 }
+
+header("HTTP/1.1 404 Not Found");
 echo renderfile(__DIR__ . "/pages/_404.html", false);
